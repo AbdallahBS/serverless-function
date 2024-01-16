295bc6f33e41ed97b4a5c7a9fba2fae9a4fbf833
Abdallah Ben Salem <90615628+AbdallahBS@users.noreply.github.com>
2024-01-16 19:17:06 +0100

    new changes

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -1,9 +1,13 @@
-// domaine/.netlify/1-hello
-const items = [ {name:'susan'},{name:'anna'}]
-const items2 = require('../assets/data')
-exports.handler = async (event,context)=>{
+const { handler } = require('netlify-lambda');
+const items = [{ name: 'susan' }, { name: 'anna' }];
+const items2 = require('../assets/data');
+const nodemailer = require('nodemailer');
+
+exports.handler = async (event, context) => {
+  // Log the error for debugging
     return {
-        statusCode:200,
-        body:JSON.stringify(items2),
-    }
+      statusCode: 500,
+      body: "good"
+    };
+ 
 }
